01465f7515a5cd75b3fe90d3059b229ab98e68fb
Yuning Jin <yuningj@yahoo-inc.com>
2017-12-21 11:23:06 +0800

    archived

--- NOC_ARCHIVED/NOC_app.js
+++ NOC_ARCHIVED/NOC_app.js
@@ -1,8 +1,9 @@
 //app.js
 App({
   onLaunch: function () {
-    // local storage
+    // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
     // 登录
